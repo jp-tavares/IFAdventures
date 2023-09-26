50382bd2a534e603469455601fa7534a2d441029
Tavares <jptavares.dev@gmail.com>
2023-09-26 01:00:23 -0300

    feat: adiciona interação WIP

---
+++ Assets/02.Script/Gameplay/DialogManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    [SerializeField] GameObject dialogBox;
+    [SerializeField] Text dialogText;
+
+    public void ShowDialog()
+    {
+
+    }
+}
